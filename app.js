464a02aad36ce5fa15b7c4d6620433ade60fc034
tgtiburon <tg.tiburon@gmail.com>
2021-11-16 16:21:32 -0500

    conditional question with inquirer

--- app.js
+++ app.js
@@ -53,9 +53,26 @@ const promptUser = () => {
             }  
         },
         {
+            type:'confirm',
+            name:'confirmAbout',
+            message: 'Would you like to enter some information about yourself for an "About" section?',
+            default: true
+
+        },
+        {
             type: 'input',
             name: 'about',
-            message: 'Provide some information about yourself:'
+            message: 'Provide some information about yourself:',
+            when: ({confirmAbout}) => {
+                // conditional statement based on answers already
+                // given
+                if(confirmAbout) {
+                    return true;
+
+                } else {
+                    return false;
+                }
+            }
         }
     ]);
     
